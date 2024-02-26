f768707b925cad0f3f68f937ba736c7b0d2d7b1f
mstwsk <e.mstwsk@gmail.com>
2024-02-26 08:25:20 +0000

    Added all Sandbox files to repository

---
+++ src/index.js
@@ -0,0 +1,20 @@
+function displayintroData(response) {
+  let cityName = response.data.city;
+  let cityHeading = document.querySelector("#current-city");
+  cityHeading.innerHTML = cityName;
+
+  let currentTemperature = document.querySelector("#today-temperature");
+  currentTemperature.innerHTML =
+    Math.round(response.data.temperature.current) + "°";
+}
+
+document
+  .querySelector("#search-city")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let cityName = document.querySelector("#enter-city").value;
+    let apiKey = "46df0bc13abce7403ee4b936f24tbeoa";
+    let weatherUnit = "metric";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}&units=${weatherUnit}`;
+    axios.get(apiURL).then(displayintroData);
+  });
