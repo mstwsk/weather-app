388940c32d906b17e8f3b8270e7c796443a9ab23
mstwsk <e.mstwsk@gmail.com>
2024-03-12 08:12:29 +0000

    Update full js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-function displayintroData(response) {
-  let cityName = response.data.city;
-  let cityHeading = document.querySelector("#current-city");
-  cityHeading.innerHTML = cityName;
-
+function refreshWeather(response) {
   let currentTemperature = document.querySelector("#today-temperature");
   currentTemperature.innerHTML =
     Math.round(response.data.temperature.current) + "°";
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
 }
 function searchCity(city) {
   let apiKey = "46df0bc13abce7403ee4b936f24tbeoa";
-  let weatherUnit = "metric";
-  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}&units=${weatherUnit}`;
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiURL).then(displayintroData);
 }
+
+function searchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#enter-city");
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-city");
+searchFormElement.addEventListener("Submit", searchSubmit);
+
+searchCity("London");
