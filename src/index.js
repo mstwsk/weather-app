6b46e1575a6369f7e6097752e1226b796c9a5b73
mstwsk <e.mstwsk@gmail.com>
2024-03-12 07:53:15 +0000

    changed to function to reflect shecodes js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,9 @@ function displayintroData(response) {
   currentTemperature.innerHTML =
     Math.round(response.data.temperature.current) + "°";
 }
-
-document
-  .querySelector("#search-city")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    let cityName = document.querySelector("#enter-city").value;
-    let apiKey = "46df0bc13abce7403ee4b936f24tbeoa";
-    let weatherUnit = "metric";
-    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}&units=${weatherUnit}`;
-    axios.get(apiURL).then(displayintroData);
-  });
-
-displayintroData("London");
+function searchCity(city) {
+  let apiKey = "46df0bc13abce7403ee4b936f24tbeoa";
+  let weatherUnit = "metric";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${cityName}&key=${apiKey}&units=${weatherUnit}`;
+  axios.get(apiURL).then(displayintroData);
+}
