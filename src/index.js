ec7b041286487957178ad449a0fe0f2f23d5aaca
mstwsk <e.mstwsk@gmail.com>
2024-03-16 09:49:06 +0000

    added date to js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Function to update weather data on the page
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#today-temperature");
   let temperature = response.data.temperature.current;
@@ -8,9 +9,17 @@ function updateWeather(response) {
   let timeElement = document.querySelector("#current-time");
   let time = new Date(response.data.time * 1000);
   let weekdayElement = document.querySelector("#current-day");
-  let weekday = new Date(response.data.time);
 
-  console.log(response.data);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let today = new Date();
 
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature) + "°";
@@ -18,21 +27,44 @@ function updateWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeElement.innerHTML = `${time.getHours()}:${time.getMinutes()}`;
-  weekdayElement.innerHTML = `${weekday.getDay}`;
+  weekdayElement.innerHTML = days[today.getDay()]; // Update with current day
 }
+
+// Function to format date
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let weekday = days[date.getDay()];
+  // Return formatted date
+  return `${weekday}, ${hours}:${minutes}`;
+}
+
+// Function to fetch weather data based on city
 function searchCity(city) {
   let apiKey = "46df0bc13abce7403ee4b936f24tbeoa";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiURL).then(updateWeather);
 }
 
+// Function to handle form submission
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
 }
 
+// Add event listener to search form
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
+// Initial search for London
 searchCity("London");
